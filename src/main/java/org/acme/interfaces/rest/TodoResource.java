bb7d0707164df61751f51af17d52dc305e14ccc6
ikerMJHDZ09 <A01352358@tec.mx>
2026-04-13 17:08:01 -0600

    Revert "feat(todo): add DELETE /todos/{id} endpoint"

--- src/main/java/org/acme/interfaces/rest/TodoResource.java
+++ src/main/java/org/acme/interfaces/rest/TodoResource.java
@@ -1,9 +1,7 @@
 package org.acme.interfaces.rest;
 
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -27,19 +25,7 @@ public class TodoResource {
 
     @POST
     public Response createTodo(CreateTodoDto todoDto) {
-        Todo todo = createTodoUseCase.execute(todoDto);
+        Todo todo= createTodoUseCase.execute(todoDto);
         return Response.ok(todo).build();
-    }
-
-    @DELETE
-    @Path("/{id}")
-    @Transactional
-    public Response delete(@PathParam("id") Long id) {
-        // 🐛 BUG INTENCIONAL: ignoramos el id recibido
-        // y siempre borramos el ToDo con id = 1
-        boolean deleted = repo.deleteById(1L);
-        return deleted
-                ? Response.noContent().build()
-                : Response.status(404).build();
     }
 }
